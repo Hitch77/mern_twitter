8557293247139156d53394f7308bee3a9e5ed167
Hicham <>
2021-10-10 19:26:08 -0400

    setup

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://dev:B0MDTNX1jrSG0BAK@cluster0.gbgoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+            //Make sure this is your own unique string
+}
