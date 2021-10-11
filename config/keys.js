6d15f1bec04600e9414e475e79f73359e27a5786
Hicham <>
2021-10-11 04:06:20 -0400

    mern push

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://dev:B0MDTNX1jrSG0BAK@cluster0.gbgoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
+  mongoURI: 'mongodb+srv://dev:B0MDTNX1jrSG0BAK@cluster0.gbgoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
             //Make sure this is your own unique string
 }
